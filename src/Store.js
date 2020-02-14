499b67c32e85ca8518339034fd596c4957220cc0
Tera15 <ccatherall15@gmail.com>
2020-02-14 01:29:06 -0800

    chatroom

---
+++ src/Store.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+//initialize React Context 
+export const CTX = React.createContext();
+
+/**
+ *{
+     from: 'user',
+     msg: 'hi',
+     topic: 'general
+ } 
+ *
+
+
+ state {
+     General [
+         {msg}, {msg}, {msg}
+     ]
+
+     topic2: [
+         {msg}, {msg}, {msg}
+     ]
+ }
+ */
+const initState = {
+    General: [
+        // {from: 'Aaron', msg: 'Hey buddy'},
+        // {from: 'Bill', msg: 'Hey buddy'},
+        // {from: 'Ronald', msg: 'Hey buddy'}
+    ],
+
+    RoomTwo: [
+        // {from: 'Arnold', msg: 'Hey buddy'},
+        // {from: 'Ben', msg: 'Hey buddy'},
+        // {from: 'Roy', msg: 'Hey buddy'}
+    ]
+}
+
+
+function reducer(state, action) {
+    const {from, msg, topic} = action.payload;
+   switch(action.type) {
+       case 'RECIEVE_MESSAGE':
+           return{
+               ...state,
+               [topic]: [
+                   ...state[topic],
+                   {from, msg}
+               ]
+           }
+        default: 
+            return state   
+   }
+}
+
+//initialized socket outside of Store 
+// so that socket doesnt rerender every store reload
+let socket;
+
+
+
+function sendChatAction(value){
+    socket.emit('chat message', value);
+}
+
+export default function Store(props) {
+    const [allChats, dispatch] = React.useReducer(reducer, initState);
+    if (!socket) {
+        socket = io(":3001");
+        socket.on("chat message", function(msg) {
+            console.log(msg)
+          dispatch({ type: 'RECIEVE_MESSAGE', payload: msg });
+        });
+      }
+    
+      const user = 'user' + Math.random(100).toFixed();
+    
+     return (
+        <CTX.Provider value={{allChats, sendChatAction, user}}>
+            {props.children}
+        </CTX.Provider>
+    );
+}
